42b7eddd1686bfd336c79bbcfb9f075550523c6f
Saren Currie <saren@sarencurrie.com>
2016-08-12 13:39:23 +1200

    Added matrial ui, with working example.

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 
-class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World!!!
-         </div>
-      );
-   }
+var alertTest = function () {
+  alert('hello');
+};
+
+class AlertButton extends React.Component {
+  handleTouchTap() {
+    alert('hello');
+  }
+
+  render() {
+    return <RaisedButton label="Default" onTouchTap={this.handleTouchTap} />;
+  }
 }
 
-export default App;
+export default AlertButton;
--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './components/App.jsx';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const App = () => (
+  <MuiThemeProvider>
+    <MyAwesomeReactComponent />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
