fd0f0edd16f89bd538d1df7cb84cb21a579fa7ce
Dean Ulbrick <d.d.ulbrick@gmail.com>
2016-08-13 12:28:45 +1200

    Post item should now work
    Post needed the adding of a converter to convert JSON objects posted to
    either DtoNode or DtoLeaf. DotNet magic allows this to happen without me
    changing the actual controller class. The objects sent from the client now
    need a a type field to discriminate, but that can be easily changed.
    Help from the Salt Monster Frano.

--- Backend/Coevolution/Coevolution/Models/DtoItem.cs
+++ Backend/Coevolution/Coevolution/Models/DtoItem.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Coevolution.Models
 {
+    [JsonConverter(typeof(DtoItemJsonConverter))]
     public class DtoItem
     {
         public int? Id { get; set; }
@@ -24,6 +28,30 @@ namespace Coevolution.Models
 
         public virtual Item ToDomainObject(Node parent){
             throw new NotImplementedException("ToDomainObject should only be called on subclasses.");
+        }
+
+        private class DtoItemJsonConverter : JsonCreationConverter<DtoItem>
+        {
+            protected override DtoItem Create(Type objectType, JObject jObject)
+            {
+                var type = jObject.Value<string>("Type");
+                if (type == null)
+                {
+                    throw new InvalidDataException("DtoItem sent by client must have non-null type field."); // TODO: Throw 4XX rather than 5XX
+                }
+                else if (type.Equals("node"))
+                {
+                    return new DtoNode();
+                }
+                else if (type.Equals("leaf"))
+                {
+                    return new DtoLeaf();
+                }
+                else
+                {
+                    throw new InvalidDataException("DtoItem sent by client must have type field set to 'node' or 'leaf'."); // TODO: Throw 4XX rather than 5XX
+                }
+            }
         }
     }
 }
