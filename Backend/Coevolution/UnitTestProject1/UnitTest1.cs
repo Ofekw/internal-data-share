6a8494579ab1000d8b5a24397e006549455f2c2e
Dean Ulbrick <d.d.ulbrick@gmail.com>
2016-08-19 17:31:34 +1200

    Added first REST API test to the test project
    Currently should post and then check that the post returned created
    status.

--- Backend/Coevolution/UnitTestProject1/UnitTest1.cs
+++ Backend/Coevolution/UnitTestProject1/UnitTest1.cs
@@ -1,36 +1,46 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+using System.Net.Http.Headers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Coevolution.Models;
+
+using Coevolution.Controllers;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+
+        private String jsonString = "{\"Type\": \"node\",\"key\":\"The first thing created by the test!\"}";
+
         [TestMethod]
         public void TestMethod1()
         {
-            using (var db = new ModelContext())
+            var two = typeof(ItemsController).Assembly;
+            HttpSelfHostConfiguration config = new HttpSelfHostConfiguration("http://localhost:55426");
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}");
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            using (HttpClient client = new HttpClient())
             {
-                var node = new Node() { Key = "TheNode" };
-                var leaf = new Leaf() { Key = "TheLeaf", Value = "TheValue" };
-
-                var label = new Label() { Item = node, Content = "This is a Label" };
-                var note = new Note() { Item = node, Content = "This is a Note" };
-
-                node.Children.Add(leaf);
-                leaf.Parent = node;
-
-                node.Labels.Add(label);
-                node.Notes.Add(note);
-
-                db.Items.Add(node);
-                db.Items.Add(leaf);
-                db.Labels.Add(label);
-                db.Notes.Add(note);
-
-                db.SaveChanges();
-            }
+                server.OpenAsync().Wait();
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:55426/api/items");
+                var content = new StringContent(jsonString);
+                content.Headers.ContentType = new MediaTypeHeaderValue("text/json");
+                request.Content = content;
+                using (request)
+                using (HttpResponseMessage response = client.SendAsync(request).Result)
+                {
+                    var task = response.Content.ReadAsStringAsync();
+                    task.Wait();
+                    Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+                }
+                server.CloseAsync().Wait();
+            };
         }
     }
 }
