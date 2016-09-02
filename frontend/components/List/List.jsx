ab34e9ba3c56b3cda766ee51fb7b3842ae4dbf08
Greggory Tan <greggory_737@live.com>
2016-09-02 15:36:34 +1200

    Added Delete/Undo ui function.

--- frontend/components/List/List.jsx
+++ frontend/components/List/List.jsx
@@ -1,27 +1,54 @@
 import React from 'react';
 import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
-import DeleteIcon from 'material-ui/svg-icons/action/delete';
+import Delete from 'material-ui/svg-icons/action/delete';
+import Undo from 'material-ui/svg-icons/content/undo';
+import RaisedButton from 'material-ui/RaisedButton';
 
 var ListComponent = React.createClass({
 
 	onClick: function (item){
+		if (this.props.editable){
+			return
+		}
 		this.props.handleClick(item);
 	},
 
-	render: function(){
-		var removeIcon;
-		if(this.props.editable){
-			removeIcon = <DeleteIcon/>;
-		} else {
-			removeIcon = null;
+	onDeleteItem: function(id){
+		var del = this.state.delete;
+		if (del.indexOf(id) > -1){
+			del.splice(del.indexOf(id), 1);
 		}
+		else{
+			del.push(id);
+		}
+		this.setState({delete: del});
+	},
+
+	getInitialState: function(){
+		return {delete: []}
+	},
+
+	render: function(){
+		var buttonStyle = {
+			float: 'right',
+			marginTop: 5 
+		};
+
 		return (
 			<List>
 				<Subheader>Children Nodes</Subheader>
 				{
 					this.props.listItems.map( item => {
-						return <ListItem primaryText={item.Key} key={item.Id} onClick={this.onClick.bind(this,item)} rightIconButton={removeIcon}/>
+						var icon = <Delete />;
+						var style={};
+						if (this.state.delete.indexOf(item.Id) > -1){
+							icon = <Undo />
+							style = {backgroundColor: '#ddd'}
+						}
+						return <ListItem primaryText={item.Key} key={item.Id} disabled={this.props.editable} onClick={this.onClick.bind(this,item)} style={style}
+									rightIconButton={this.props.editable ? <RaisedButton icon={icon} style={buttonStyle} onTouchTap={this.onDeleteItem.bind(this, item.Id)}/> : null
+								}/>
 					})
 				}
 			</List>
